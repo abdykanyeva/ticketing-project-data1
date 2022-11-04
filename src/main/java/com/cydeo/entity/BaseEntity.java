fb8bba97986157a0f899c10fe49f3a7afdf59028
Adelina <adelina.abdykanyeva@gmail.com>
2022-11-04 16:04:59 -0500

    part-3

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -23,4 +23,21 @@ public class BaseEntity {
 
 
 
+    @PrePersist
+    private void onPrePersist(){
+        this.insertDateTime = LocalDateTime.now();
+        this.lastUpdateDateTime = LocalDateTime.now();
+        this.insertUserId=1L;
+        this.lastUpdateUserId = 1L;
+
+    }
+
+    @PreUpdate
+    private void onPreUpdate(){
+        this.lastUpdateDateTime = LocalDateTime.now();
+        this.lastUpdateUserId=1L;
+    }
+
+
+
 }
