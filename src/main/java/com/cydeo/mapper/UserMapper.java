6eed64c83653f5f6b28389271f4410f3249e1771
Adelina <adelina.abdykanyeva@gmail.com>
2022-11-03 20:27:13 -0500

    part-2

---
+++ src/main/java/com/cydeo/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package com.cydeo.mapper;
+
+import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
+import org.modelmapper.ModelMapper;
+
+public class UserMapper {
+
+    private ModelMapper modelMapper;
+
+
+    public UserMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public User convertToEntity(UserDTO userDTO){
+       return modelMapper.map(userDTO, User.class);
+    }
+
+
+    public UserDTO convertToDto(User user){
+        return modelMapper.map(user, UserDTO.class);
+    }
+
+
+
+}
